63b66bd6df8238847c157b76c7fd4828e80b6395
karnveergill <karnveerg94@gmail.com>
2026-08-10 03:13:51 +0000

    test Vector3 Magnitude()

--- apps/vector3_demo.cpp
+++ apps/vector3_demo.cpp
@@ -10,9 +10,10 @@ int main()
     Vector3 position_0(10.2, 5, 30); // Initial position [meters]
     Vector3 velocity(4.75, 2.0, 8);  // Obejct velocity [meters/second]
 
-    // Set initial position and print
+    // Set initial position and print info / starting distance from center
     Vector3 prev_position = position_0;
     std::cout << "Starting position: " << prev_position << std::endl; 
+    std::cout << "Starting Distance from Center: " << prev_position.Magnitude() << std::endl;
 
     // Print position over time 
     double dt = 0.5;   // .5 second intervals
@@ -21,12 +22,14 @@ int main()
     {
         Vector3 update = prev_position + velocity*dt;
         prev_position = update;
-        std::cout << "Time " << dt*i << ": " << prev_position << std::endl;
+        std::cout << "Time " << dt*i << ": " << prev_position 
+                  << " Distance from Center: " << prev_position.Magnitude() << std::endl;
     }
 
     // Confirm Final position
     Vector3 check = position_0 + velocity*(dt*steps);
-    std::cout << "Should be same as final time: " << check << std::endl;  
+    std::cout << "Should be same as final time: " 
+              << check << " Dist: " << check.Magnitude() << std::endl;  
 
     return 0; 
 }
