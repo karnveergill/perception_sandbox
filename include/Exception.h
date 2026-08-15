e9748ef25ac41d912196aa84d093954cc1dbc729
karnveergill <karnveerg94@gmail.com>
2026-08-15 09:08:47 +0000

    just let std::string handle memory clean up in Exception

--- include/Exception.h
+++ include/Exception.h
@@ -13,39 +13,38 @@ public:
      * @brief Constructor builds exception with a printf style c string and args 
      */
     Exception(const char* format, ...)
-        : m_error_msg(format)
     {
         // Tool to grab pointer to additional args from ... and iterate through 
         va_list args;
-        // Start tool and look for all arguments after format string
+
+        // Start tool build string dry run in nullptr to get buffer length
         va_start(args, format);
-        // Dry run building the string in a nullptr and get buffer length
         int length = vsnprintf(nullptr, 0, format, args) + 1;
-        // Clean up tool
         va_end(args);
 
-        // Dynamically allocate memory for buffer
-        char* buffer = new char[length];
+        // Handle formatting failure
+        if(length < 0)
+        {
+            m_error_msg = "Exception printf style formatting failed";
+            return;
+        }
 
-        // Restart va_list for actual use
+        // Allocate buffer of size length + null terminator
+        std::string buffer(length + 1, '\0');
+
+        // Restart va_list for actual use and create formatted message
         va_start(args, format);
-        vsnprintf(buffer, length, format, args);
+        vsnprintf(buffer.data(), buffer.size(), format, args);
         va_end(args);
 
-        // Save message and free dynamically allocated buffer
+        // Save message 
         m_error_msg = buffer;
-        delete[] buffer; 
     }
-
-    /**
-     * @brief Destructor
-     */
-    ~Exception() _NOEXCEPT {}
 
     /*
      * @brief Retrieve 'what' the exception is
      */
-    const char* what() const _NOEXCEPT override
+    const char* what() const noexcept override
     {
         return m_error_msg.c_str();
     }
