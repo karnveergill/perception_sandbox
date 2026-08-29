1b6c6c5edbfe1656dd4647c38e820e91dcf2298f
karnveergill <karnveerg94@gmail.com>
2026-08-29 18:53:54 +0000

    checkout real and int distrubtuions

--- apps/random_demo.cpp
+++ apps/random_demo.cpp
@@ -9,10 +9,21 @@ int main()
     Random rand; 
     Random rand42(42);
     
-    
+    cout << "Uniform Real Distrubution from -1 to 1" << endl;
+    int min = -1, max = 1; 
     for(int i = 0; i < 10; ++i)
     {
-        cout << i <<  " - Rando: " << rand.Uniform(-1,1) << " Rando 42: " << rand42.Uniform(-1,1) << endl;
+        cout << i <<  " - Rando: " << rand.Uniform(min,max) << 
+                        " Rando 42: " << rand42.Uniform(min,max) << endl;
+    }
+
+    cout << endl;
+    cout << "Uniform Integer Distrubution from -10 to 10" << endl;
+    min = -10, max = 10; 
+    for(int i = 0; i < 10; ++i)
+    {
+        cout << i <<  " - Rando: " << rand.Uniform_int(min,max) << 
+                        " Rando 42: " << rand42.Uniform_int(min,max) << endl;
     }
     return 0;
 }
